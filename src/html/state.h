c34b9a8d0def00e7db5d7d5e16b795c18b810e47
xi4f3i <xi4f3i@gmail.com>
2026-01-05 20:31:12 +0800

    Add HTML tokenizer and char util

---
+++ src/html/state.h
@@ -0,0 +1,18 @@
+#pragma once
+
+enum class State {
+  Data,
+  TagOpen,
+  EndTagOpen,
+  TagName,
+  BeforeAttributeName,
+  AttributeName,
+  AfterAttributeName,
+  BeforeAttributeValue,
+  AttributeValueUnquoted,
+  AttributeValueDoubleQuoted,
+  AttributeValueSingleQuoted,
+  AfterAttributeValueQuoted,
+  SelfClosingStartTag,
+  Comment,
+};
